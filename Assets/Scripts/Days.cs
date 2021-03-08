3dcb359a5c291c3090473c688e0346c06f789ad4
Agustin Pontura <pontura@gmail.com>
2021-03-08 10:37:46 -0300

    solo día 1

--- Assets/Scripts/Days.cs
+++ Assets/Scripts/Days.cs
@@ -12,12 +12,15 @@ public class Days : ScreenMain
         Events.SetBackButton(false);
         Events.SetNextButton(false);
         Utils.RemoveAllChildsIn(container);
+        int id = 0;
         foreach(DaysData.Content content in Data.Instance.daysData.content)
         {
             DayButton newButton = Instantiate(button);
             newButton.transform.SetParent(container);
             newButton.transform.localScale = Vector2.one;
             newButton.Init(this, content);
+            id++;
+            if (id > 0) return;
         }
     }
     public void OnSelected(DaysData.Content content)
