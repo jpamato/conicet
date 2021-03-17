de07f2f6d9c55a3093aa0d39eb3033ba1df39c8b
Agustin Pontura <pontura@gmail.com>
2021-03-17 16:37:14 -0300

    dataFolder

---
+++ Assets/MainMenuPopup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuPopup : MonoBehaviour
+{
+    public GameObject panel;
+
+    void Start()
+    {
+        Close();
+    }
+    public void Open()
+    {
+        panel.SetActive(true);
+    }
+    public void Close()
+    {
+        panel.SetActive(false);
+    }
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+}
+
