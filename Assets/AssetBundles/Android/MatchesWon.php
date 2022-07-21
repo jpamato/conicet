d9c649c9e9760340a44a2ca6a5ac88869de37a41
BrendaBurgoa <61566158+BrendaBurgoa@users.noreply.github.com>
2022-07-21 17:55:21 -0300

    Cuento 1 Q2

---
+++ Assets/AssetBundles/Android/MatchesWon.php
@@ -0,0 +1,129 @@
+<!DOCTYPE  html>
+
+<?php 
+$fm = date('m',strtotime('-1 Monday'));
+$fd = date('d',strtotime('-1 Monday'));
+$fyear = date('Y',strtotime('-1 Monday'));
+$tm = date('m');
+$td = date('d');
+$tyear = date('Y');
+
+/*
+$fm = "05";
+$fd = "01";
+$tyear = "2023";
+$tm = "03";
+$td = "01";
+*/
+
+if(isset($_REQUEST["fm"]))
+{
+	$fm = $_REQUEST["fm"];
+	$fd = $_REQUEST["fd"];
+	$tm = $_REQUEST["tm"];
+	$td = $_REQUEST["td"];
+	$tyear = $_REQUEST["tyear"];
+}
+include ("admin/configurations/connect.php");
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ranking by Won Matches - Fulbo Galaxy</title>
+    <meta name="description" content="Home page for Fulbo Galaxy, the DeFi Revolution" />
+    <meta name="author" content="Aconcagua Games" />
+    <link rel="icon" href="/logo.png" type="image/png" />	
+	<meta charset="UTF-8">
+	<link href="styles/ranking_style.css" rel="stylesheet" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	</head>
+	<body>
+        <div class="main">
+            <div class="container">
+                <!-- Ranking -->
+                <div class="ranking-title">				
+					<div class="back"><i class="fa fa-arrow-left"></i><a href = "Rankings.php">All Rankings</a></div>
+                    <h1>Ranking by Won Matches (only for Twitter Users)</h1>
+                </div>
+				 <div class="ranking-data">
+                    <h2>From: <span><?php echo $fyear; ?>-<?php echo $fm; ?>-<?php echo $fd; ?></span></h2>
+                    <h2>To: <span><?php echo $tyear; ?>-<?php echo $tm; ?>-<?php echo $td; ?></span></h2>
+                </div>
+                <!-- Tabla Posiciones -->
+                <div class="position-table">
+                    <table>
+                        <tr>
+                          <th>#</th>
+                          <th>Twitter</th>
+                          <th>Team</th>
+                          <th>Total Won</th>
+                        </tr>
+						
+						<?php
+$query = "SELECT 
+	users.twitter,
+    users.email,
+    users.user,
+	tracking.score_team1,
+	tracking.score_team2,   
+    COUNT(*) as totalMatches
+FROM 
+	tracking
+INNER JOIN	
+	users ON users.email = tracking.email	
+WHERE 
+	tracking.timestamp BETWEEN '" . $fyear . "-" . $fm . "-" . $fd . " 00:00:01' AND '" . $tyear . "-" . $tm . "-" . $td . " 23:59:59'
+	AND tracking.score_team2 > tracking.score_team1 
+GROUP BY 
+	tracking.email
+ORDER BY
+	totalMatches DESC
+LIMIT 
+	100;";
+
+$statement=$pdo->prepare($query); 
+$statement->execute(); 
+$num = 1;
+if (!$statement){ 
+	echo 'Error al ejecutar la consulta'; 
+}else{ 
+	$array = $statement->fetchAll( PDO::FETCH_ASSOC );
+	foreach($array as $row) {
+		if($row["twitter"] != "")
+		{	
+		$twitterName = $row["twitter"];
+		if($twitterName[0] != "@")
+			$twitterName = "@" . $twitterName;
+					?>
+				
+                        <tr>
+                          <td><?php echo $num; ?></td>
+                          <td><?php echo $twitterName ?></td>
+                          <td><?php echo $row["user"]; ?></td>
+                          <td><?php echo $row["totalMatches"]; ?></td>			  
+                        </tr>
+	<?php 
+		$num++;
+		} 
+	}
+}
+?>
+                      </table>
+                </div>
+            </div>
+        </div>
+        <!-- Imagenes -->
+        <div class="imagen_animacion izquierda">
+            <img src="images/rocketkim.png" alt="Rocket" style="width: 80%;">
+        </div>
+        <div class="imagen_animacion derecha">
+            <img src="images/rocketgirl.png" alt="Rocket" style="transform: rotate(-10deg);">
+        </div>
+	</body>
+</html>
+
+
---
+++ Assets/AssetBundles/Android/Rankings.php
@@ -0,0 +1,42 @@
+<!DOCTYPE  html>
+</script>
+<html>
+	<head>
+		<title>Rankings</title>
+        <meta charset="UTF-8">
+        <link href="styles/ranking_style.css" rel="stylesheet" type="text/css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+        <div class="main">
+            <div class="container">
+                <!-- Ranking -->
+                <div class="ranking-title">
+                    <h1>Rankings</h1>
+                </div>
+                <div class="ranking-data">
+					<ul>
+						<li>
+							<a href="GlobalRanking.php">Global Score</a>
+						</li>
+						<li>
+							<a href="MatchesWon.php">Global Score</a>
+						</li>					
+						<li>
+							<a href="Coins.php">Coins Grabbed</a>
+						</li>
+					</ul>
+                </div>
+            </div>
+        </div>
+        <!-- Imagenes -->
+        <div class="imagen_animacion izquierda">
+            <img src="images/rocketkim.png" alt="Rocket" style="width: 80%;">
+        </div>
+        <div class="imagen_animacion derecha">
+            <img src="images/rocketgirl.png" alt="Rocket" style="transform: rotate(-10deg);">
+        </div>
+	</body>
+</html>
+
+
